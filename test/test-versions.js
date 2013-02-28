24131279c32820c5ea5f3d48ce6218faee641e54
Brian Warner <warner@lothar.com>
2013-02-28 14:21:39 -0800

    tests passing now

--- test/test-versions.js
+++ test/test-versions.js
@@ -10,42 +10,46 @@ exports["test basics"] = function(assert, done) {
     nv1.setKV("key3", "value3");
     var v1 = nv1.close();
     assert.ok(v1);
-    console.log("v1", v1);
-    console.log("verhash", v1.getVerhash());
+    L.log("v1", v1);
+    L.log("verhash", v1.getVerhash());
     assert.equal(v1.getSeqnum(), 1);
-    console.log("signed", v1.getSignedVerhash());
-    console.log("v1 again", v1);
+    L.log("signed", v1.getSignedVerhash());
+    L.log("v1 again", v1);
 
-    assert.deepEqual(v1.iterKVs().sort(),
-                     [["key1", "value1"],
-                      ["key2", "value2"],
-                      ["key3", "value3"]].sort());
-    assert.deepEqual(v1.iterKEVs().sort(),
-                     [["key1", "encrypted:value1"],
-                      ["key2", "encrypted:value2"],
-                      ["key3", "encrypted:value3"]].sort());
+    const expectedV1 = [["key1", "value1"],
+                        ["key2", "value2"],
+                        ["key3", "value3"]];
+    const expectedEV1 = [["key1", "encrypted:value1"],
+                         ["key2", "encrypted:value2"],
+                         ["key3", "encrypted:value3"]];
+    assert.deepEqual(v1.iterKVs().sort(), expectedV1.sort());
+    assert.deepEqual(v1.iterKEVs().sort(), expectedEV1.sort());
 
 
     // outbound
     var nv2 = v1.createNextVersion();
+    L.log("nv2 is", nv2);
+    L.log("v1 is still", v1);
     nv2.deleteKey("key1");
     nv2.setKV("key3", "newvalue3");
     nv2.setKV("key4", "value4");
     var v2 = nv2.close();
-    console.log(v2);
+    L.log("v2 is", v2);
 
-    assert.deepEqual(v2.iterKVs().sort(),
-                     [["key2", "value2"],
-                      ["key3", "newvalue3"],
-                      ["key4", "value4"]].sort());
-    assert.deepEqual(v2.iterKEVs().sort(),
-                     [["key2", "encrypted:value2"],
-                      ["key3", "encrypted:newvalue3"],
-                      ["key4", "encrypted:value4"]].sort());
+    assert.deepEqual(v1.iterKVs().sort(), expectedV1.sort());
+    assert.deepEqual(v1.iterKEVs().sort(), expectedEV1.sort());
+    const expectedV2 = [["key2", "value2"],
+                        ["key3", "newvalue3"],
+                        ["key4", "value4"]];
+    const expectedEV2 = [["key2", "encrypted:value2"],
+                         ["key3", "encrypted:newvalue3"],
+                         ["key4", "encrypted:value4"]];
+    assert.deepEqual(v2.iterKVs().sort(), expectedV2.sort());
+    assert.deepEqual(v2.iterKEVs().sort(), expectedEV2.sort());
 
 
     var delta1to2 = v2.createDeltaFrom(v1);
-    console.log(delta1to2);
+    L.log(delta1to2);
     assert.deepEqual(delta1to2.sort(),
                      [["del", "key1"],
                       ["set", "key3", "encrypted:newvalue3"],
@@ -53,18 +57,22 @@ exports["test basics"] = function(assert, done) {
 
     // inbound
     var nv2a = v1.createNewVersion(v2.getSignedVerhash());
-    for (let delta in delta1to2) {
+    L.log("nv2a", nv2a);
+    for (let delta of delta1to2) {
         if (delta[0] == "del")
             nv2a.deleteKey(delta[1]);
         else if (delta[0] == "set")
-            nv2a.setKVE(delta[1], delta[2]);
+            nv2a.setKEV(delta[1], delta[2]);
     }
     var v2a = nv2a.close();
-    console.log(v2a);
-    assert.deepEqual(v2a.iterKVs().sort(),
-                     [["key2", "value2"],
-                      ["key3", "newvalue3"],
-                      ["key4", "value4"]].sort());
+    L.log("v2a", v2a);
+
+    assert.deepEqual(v2a.iterKVs().sort(), expectedV2.sort());
+    assert.deepEqual(v2a.iterKEVs().sort(), expectedEV2.sort());
+    assert.deepEqual(v2.iterKVs().sort(), expectedV2.sort());
+    assert.deepEqual(v2.iterKEVs().sort(), expectedEV2.sort());
+    assert.deepEqual(v1.iterKVs().sort(), expectedV1.sort());
+    assert.deepEqual(v1.iterKEVs().sort(), expectedEV1.sort());
 
     /*
     assert.ok(1);
